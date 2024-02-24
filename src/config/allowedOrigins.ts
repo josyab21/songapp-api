786d5a54eb4cac876b5319995d33d363da92372b
Yosef <you@example.com>
2024-02-24 19:28:08 +0300

    change alloedOrigins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
   "https://songapp-api.onrender.com",
+  "https://songapp-fh9n.onrender.com",
   "http://localhost:3000",
   "http://localhost:3001",
   "http://127.0.0.1:5000",
